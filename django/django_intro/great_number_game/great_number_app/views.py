a6a7e33341b0345181d10c816c97eca6657f82da
Khalil Abuayyash <khalilayyash@gmail.com>
2021-05-22 19:17:40 +0300

    python day 8

--- django/django_intro/great_number_game/great_number_app/views.py
+++ django/django_intro/great_number_game/great_number_app/views.py
@@ -1,42 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 import random
 
 num = random.randint(1, 100)
 
 def root(request):
-    context = {
-        'text': '',
-        'color': 'white'
-    }
+    if 'text' in request.session and 'color' in request.session:
+
+        context = {
+            'text': request.session['text'],
+            'color': request.session['color']
+        }
+    else:
+        context = {
+            'text': '',
+            'color': 'white'
+        }
+
     return render(request, 'index.html',context)
 
 def compare(request):
-    context = {
-        'text': '',
-        'color': 'white'
-    }
+
     if request.method == 'POST':
         number = int(request.POST['number'])
         if number >= 0 and number <= 100:
 
             if number == num:
                 s = num, " was the number!"
-                context = {
-                    'text': s,
-                    'color': 'green'
-                }
-                return render(request, 'index.html',context)
+                request.session['color'] = 'green'
+                request.session['text'] = s
+
+                return redirect('/')
+
             elif number > num :
-                context = {
-                    'text': "Too High!",
-                    'color': 'red'
-                }
-                return render(request, 'index.html',context)
+                
+                request.session['color'] = 'red'
+                request.session['text'] = "Too High!"
+
+                return redirect('/')
+
             else:
-                context = {
-                    'text': "Too Low!",
-                    'color': 'red'
-                }
-                return render(request, 'index.html',context)
-        return render(request, 'index.html',context)
-    return render(request, 'index.html',context)
+                
+                request.session['color'] = 'red'
+                request.session['text'] = "Too Low!"
+                
+                return redirect('/')
+
+        return redirect('/')
+    return redirect('/')
