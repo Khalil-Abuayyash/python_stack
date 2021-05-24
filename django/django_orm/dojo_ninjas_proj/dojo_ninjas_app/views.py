718bcfad91b68ce874bd1ef340e4069c2d5bf3a1
Khalil Abuayyash <khalilayyash@gmail.com>
2021-05-24 17:04:02 +0300

    python day 10

--- django/django_orm/dojo_ninjas_proj/dojo_ninjas_app/views.py
+++ django/django_orm/dojo_ninjas_proj/dojo_ninjas_app/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import redirect, render, HttpResponse
+from .models import Dojo, Ninja
 
 # Create your views here.
+
 def index(request):
-    return HttpResponse('Good')
+    
+    context = {
+        'dojos': Dojo.objects.all()
+    }
+
+    return render(request, 'index.html', context)
+
+def create(request):
+    if request.method == "POST":
+        if request.POST['model'] == "dojo":
+            Dojo.objects.create(name=request.POST['name'], city=request.POST['city'], state=request.POST['state'])
+        
+        if request.POST['model'] == "ninja":
+            Ninja.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], dojo= Dojo.objects.get(id=request.POST['dojo_id']))
+    
+    return redirect('/')
